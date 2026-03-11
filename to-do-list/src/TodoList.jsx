3d61ee1b058c54a56f7aab1b5ac1b7f7a07a45be
Natnael31 <end24002byui.edu>
2026-03-11 16:51:06 +0300

    Finsihed ch-5

--- to-do-list/src/TodoList.jsx
+++ to-do-list/src/TodoList.jsx
@@ -1,10 +1,11 @@
-import { TodoItem } from "./TodoItem"
+import { TodoItem } from "./TodoItem";
 
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
     <ul className="list">
+      {/* if todo list is empty show "No Todos" */}
       {todos.length === 0 && "No Todos"}
-      {todos.map(todo => {
+      {todos.map((todo) => {
         return (
           <TodoItem
             {...todo}
@@ -12,8 +13,8 @@ export function TodoList({ todos, toggleTodo, deleteTodo }) {
             toggleTodo={toggleTodo}
             deleteTodo={deleteTodo}
           />
-        )
+        );
       })}
     </ul>
-  )
+  );
 }
